5e83302469c83a9861e75b75fafa51471ea58042
masboonnak-oss <masboonnak@gmail.com>
2026-05-09 00:58:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
-    host: '0.0.0.0',
-    headers: {
-      "Bypass-Tunnel-Reminder": "true"
-    },
-    allowedHosts: true,
-    proxy: {
-      '/api': {
-        target: 'https://fuzzy-space-rotary-phone-r7gv9rqwr97gcp4vj-3001.app.github.dev',
-        changeOrigin: true
-      },
-      '/socket.io': {
-        target: 'https://fuzzy-space-rotary-phone-r7gv9rqwr97gcp4vj-3001.app.github.dev',
-        ws: true,
-        changeOrigin: true
-      },
-    }
+    port: 5000
   }
 });
